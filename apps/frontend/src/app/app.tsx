005d954679898884e8d9871bc5f60daf06b3ff63
JESSIE YUE GUAN <jessie-yue-guan@nav.no>
2021-07-07 10:10:54 +0200

    :construction: Temporary commit

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from 'react';
 import Navbar from '../components/navbar';
 
 export const App = () => {
-  const [patientResult, setPatientResult] = useState<any>(null);
+  const [patientResult, setPatientResult] = useState<IPatient>(null);
 
   useEffect(() => {
     fetch('/api/Patient')
-      .then((r) => r.json().then(patient => setPatientResult(patient)
-      ));
+      .then((r) => r.json().then(patient => {
+        console.log(patient);
+        setPatientResult(patient);
+      }));
   }, []);
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <div style={{ textAlign: 'center' }}>
         <h1>Welcome to frontend!</h1>
         <img
@@ -21,7 +23,12 @@ export const App = () => {
           alt="Nx - Smart, Extensible Build Framework"
         />
       </div>
-      <div>Found {patientResult?.total}</div>
+      <h4>Found {patientResult?.total} patients</h4>
+      <ul>
+        {patientResult.entry.map((entry) => {
+          return <li>{entry.resource.name[0].given[0]}</li>
+        })}
+      </ul>
     </>
   );
 };
