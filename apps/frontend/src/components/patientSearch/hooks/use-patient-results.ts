1ddd8fe43649180604710f825809e398eb457968
Michael Johansen <michael.johansen@nav.no>
2021-07-29 18:01:58 +0200

    make patients searchable by name (#7)

--- apps/frontend/src/components/patientSearch/hooks/use-patient-results.ts
+++ apps/frontend/src/components/patientSearch/hooks/use-patient-results.ts
@@ -1,11 +1,18 @@
 import { fetcher } from "../../../utils";
 import useSWR from "swr";
+import validator from "@navikt/fnrvalidator";
 import { IBundle_Entry, IPatient } from "@ahryman40k/ts-fhir-types/lib/R4";
+import { stringify } from "query-string";
 
-export const usePatientResults = (identifier: string) => {
+export const usePatientResults = (searchValue: string) => {
+    const validationResult = validator.idnr(searchValue);
+    const searchKey = (validationResult.status === "valid") ? "identifier" : "name";
+    const querystring = stringify({
+        [searchKey]: searchValue,
+    });
     const { data, error } = useSWR<IBundle_Entry[]>(
-        `api/Patient?identifier=${identifier}`,
-        fetcher
+        `api/Patient?${querystring}`,
+        fetcher,
     );
 
     return {
