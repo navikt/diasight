04b0fe1739d2f1c1a78162ebf9bbca2b191b18cd
JESSIE YUE GUAN <jessie-yue-guan@nav.no>
2021-07-31 12:43:24 +0200

    💄  Finally finished with overview timeline schedule

--- apps/frontend/src/components/schedule/schedule-list.tsx
+++ apps/frontend/src/components/schedule/schedule-list.tsx
@@ -5,28 +5,40 @@ import style from "./schedule-list.module.less"
 export const ScheduleList: FC = () => {
     return (
         <div className={style.wrapper}>
-            <div className={style.appointment}>
+            <div className={style.currentTimeWrapper}>
+                <Undertekst className={style.currentTimeClock}>12:12</Undertekst>
+                <div className={style.currentTimeMarker} />
+            </div>
+            <div className={style.timeBlock}>
                 <Undertekst className={style.timeHour}>09:00</Undertekst>
-                <Element className={style.patientName}>Sofie Fagermo</Element>
-                <Undertekst className={style.patientAge}>22 år</Undertekst>
+                <div className={style.patient}>
+                    <Element className={style.patientName}>Sofie Fagermo</Element>
+                    <Undertekst className={style.patientAge}>22 år</Undertekst>
+                </div>
             </div>
-            <div className={style.appointment}>
+            <div className={style.timeBlock}>
                 <Undertekst className={style.timeHour}>10:00</Undertekst>
-                <Element className={style.patientName}>Daniel Salvesen</Element>
-                <Undertekst className={style.patientAge}>48 år</Undertekst>
+                <div className={style.patient}>
+                    <Element className={style.patientName}>Daniel Salvesen</Element>
+                    <Undertekst className={style.patientAge}>48 år</Undertekst>
+                </div>
             </div>
-            <div className={style.appointment}>
+            <div className={style.timeBlock}>
                 <Undertekst className={style.timeHour}>11:00</Undertekst>
-                <Element className={style.patientName}>Maja Ingebretsen</Element>
-                <Undertekst className={style.patientAge}>32 år</Undertekst>
+                <div className={style.patient}>
+                    <Element className={style.patientName}>Maja Ingebretsen</Element>
+                    <Undertekst className={style.patientAge}>32 år</Undertekst>
+                </div>
             </div>
-            <div className={style.appointment}>
+            <div className={style.timeBlock}>
                 <Undertekst className={style.timeHour}>12:00</Undertekst>
             </div>
-            <div className={style.appointment}>
+            <div className={style.timeBlock}>
                 <Undertekst className={style.timeHour}>13:00</Undertekst>
-                <Element className={style.patientName}>Kari Hansen</Element>
-                <Undertekst className={style.patientAge}>47 år</Undertekst>
+                <div className={style.patient}>
+                    <Element className={style.patientName}>Kari Hansen</Element>
+                    <Undertekst className={style.patientAge}>47 år</Undertekst>
+                </div>
             </div>
         </div>
     )
