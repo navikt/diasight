bd7b63205feb9373c97900c18fd7071cff173dd6
JESSIE YUE GUAN <jessie-yue-guan@nav.no>
2021-08-03 14:47:43 +0200

    🎨  Refactored overview components some more

--- apps/frontend/src/components/overview/task-overview.tsx
+++ apps/frontend/src/components/overview/task-overview.tsx
@@ -1,5 +1,4 @@
 import { ITask } from "@ahryman40k/ts-fhir-types/lib/R4";
-import { Sidetittel, Element } from "nav-frontend-typografi";
 import React from "react";
 import { FC } from "react";
 import { TaskCard } from "./task-card";
@@ -11,53 +10,16 @@ interface IProps {
 }
 
 export const TaskOverview: FC<IProps> = ({ tasks, practitionerId }) => {
-
-    const hospitalTasks = tasks ? tasks.filter(
-        (task) => task?.focus?.type === "DiagnosticReport"
-    ) : null;
-
-    const navTasks = tasks ? tasks.filter(
-        (task) => task?.requester?.reference === "Organization/12"
-    ) : null;
-
-
     return (
-        <>
-            <div className={style.dailyMessage}>
-                <Sidetittel>
-                    Geir Nystøl, i dag har du <span className={style.dynamicField}>6 pasienter</span>,
-                    <br />
-                    du har mottat <span className={style.dynamicField}>4 epikriser</span> fra sykehuset
-                    <br />
-                    og må skrive <span className={style.dynamicField}>2 legeerklæringer</span> til NAV.
-                </Sidetittel>
-            </div>
-            <div className={style.notifications}>
-                <Element>Fra sykehus</Element>
-                <div className={style.wrapper}>
-                    {hospitalTasks?.map((task, index) => {
-                        return task.id ? (
-                            <TaskCard
-                                key={index}
-                                taskId={task.id}
-                                ownerId={practitionerId} />
-                        ) : null;
-                    })}
-                </div>
-            </div>
-            <div className={style.notifications}>
-                <Element>Fra NAV</Element>
-                <div className={style.wrapper}>
-                    {navTasks?.map((task, index) => {
-                        return task.id ? (
-                            <TaskCard
-                                key={index}
-                                taskId={task.id}
-                                ownerId={practitionerId} />
-                        ) : null;
-                    })}
-                </div>
-            </div>
-        </>
+        <div className={style.wrapper}>
+            {tasks?.map((task, index) => {
+                return task.id ? (
+                    <TaskCard
+                        key={index}
+                        taskId={task.id}
+                        ownerId={practitionerId} />
+                ) : null;
+            })}
+        </div>
     )
 }
--- apps/frontend/src/layouts/overview-layout.tsx
+++ apps/frontend/src/layouts/overview-layout.tsx
@@ -1,3 +1,4 @@
+import { Sidetittel, Element } from "nav-frontend-typografi";
 import React, { FC } from "react";
 import { TaskOverview } from "../components/overview/task-overview";
 import { ScheduleList } from "../components/schedule/schedule-list";
@@ -13,13 +14,35 @@ export const OverviewLayout: FC = () => {
     if (isError) return <div>Error</div>
     if (!tasks) return <div>No data</div>
 
+    const hospitalTasks = tasks ? tasks.filter(
+        (task) => task?.focus?.type === "DiagnosticReport"
+    ) : tasks;
+
+    const navTasks = tasks ? tasks.filter(
+        (task) => task?.requester?.reference === "Organization/12"
+    ) : tasks;
+
     return (
         <div className={style.overviewWrapper}>
-            <div>
-                <TaskOverview tasks={tasks} practitionerId={practitionerId} />
+            <div className={style.dailyMessage}>
+                <Sidetittel>
+                    Geir Nystøl, i dag har du <span className={style.dynamicField}>6 pasienter</span>,
+                    <br />
+                    du har mottat <span className={style.dynamicField}>4 epikriser</span> fra sykehuset
+                    <br />
+                    og må skrive <span className={style.dynamicField}>2 legeerklæringer</span> til NAV.
+                </Sidetittel>
             </div>
-            <div>
+            <div className={style.notifications}>
+                <Element>Fra sykehus</Element>
+                <TaskOverview tasks={hospitalTasks} practitionerId={practitionerId} />
+            </div>
+            <div className={style.calendar}>
                 <ScheduleList />
+            </div>
+            <div className={style.notifications}>
+                <Element>Fra NAV</Element>
+                <TaskOverview tasks={navTasks} practitionerId={practitionerId} />
             </div>
         </div>
     );
