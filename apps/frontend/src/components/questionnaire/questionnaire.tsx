4b992f8e438ee6d27cd12580c193bddde6609795
Amund Lunke Røhne <amundlrohne@gmail.com>
2021-08-03 16:17:14 +0200

    State handling for questionnaire-response

--- apps/frontend/src/components/questionnaire/questionnaire.tsx
+++ apps/frontend/src/components/questionnaire/questionnaire.tsx
@@ -1,4 +1,8 @@
-import { IPatient } from "@ahryman40k/ts-fhir-types/lib/R4";
+import {
+    IPatient,
+    IQuestionnaireResponse,
+    IQuestionnaireResponse_Answer,
+} from "@ahryman40k/ts-fhir-types/lib/R4";
 import { Close } from "@navikt/ds-icons/cjs";
 import { Normaltekst, Systemtittel, Undertekst, Undertittel } from "nav-frontend-typografi";
 import React, { useContext, useState } from "react";
@@ -6,18 +10,42 @@ import { FC } from "react";
 import { useQuestionnaire } from "./hooks/use-questionnaire";
 import style from "./questionnaire.module.less";
 import { generateFHIRForm } from "./utils/generate-form";
+
 interface IProps {
     id: number;
     patient: IPatient;
 }
-type AnswerType = {
-    linkId: string;
-    answer: string;
+
+const initialQuestionnaireResponse: IQuestionnaireResponse = {
+    resourceType: "QuestionnaireResponse",
+    item: [],
 };
 
 export const Questionnaire: FC<IProps> = ({ id, patient }) => {
-    const [answers, setAnswers] = useState<AnswerType[]>([]);
+    const [answers, setAnswers] = useState<IQuestionnaireResponse>(initialQuestionnaireResponse);
     const { questionnaire, isLoading, isError } = useQuestionnaire(id);
+
+    const handleAnswer = (answer: IQuestionnaireResponse_Answer, linkId: string) => {
+        const updatedResponse = { ...answers };
+        const responseItem = updatedResponse.item?.find((i) => i.linkId === linkId);
+
+        if (!responseItem) {
+            updatedResponse.item?.push({ linkId: linkId, answer: [answer] });
+            setAnswers(updatedResponse);
+            return;
+        }
+
+        const filteredItems = updatedResponse.item?.filter((item) => item !== responseItem) || [];
+        responseItem.answer = [answer];
+
+        updatedResponse.item = [...filteredItems, responseItem];
+        setAnswers(updatedResponse);
+        console.log(answers);
+    };
+
+    const getAnswer = (linkId: string) => {
+        return answers.item?.find((item) => item.linkId === linkId)?.answer;
+    };
 
     const date = new Date();
 
@@ -31,13 +59,20 @@ export const Questionnaire: FC<IProps> = ({ id, patient }) => {
                     <Undertittel>{questionnaire.title}</Undertittel>
                     <Close />
                 </div>
-                <form onSubmit={() => console.log("Hallo")}>
-                    <Systemtittel>{questionnaire.title}</Systemtittel>
-                    <Normaltekst>
+                <form>
+                    <Systemtittel className={style.fullSpan}>{questionnaire.title}</Systemtittel>
+                    <Normaltekst className={style.fullSpan}>
                         {questionnaire.title} gjelder for Ola Normann født {patient.birthDate}.
                     </Normaltekst>
-                    {questionnaire.item?.map((value) => {
-                        return generateFHIRForm(value);
+                    {questionnaire.item?.map((question) => {
+                        if (!question.linkId) return null;
+                        const linkId = question.linkId;
+                        const value = getAnswer(linkId) || [];
+                        const change = (answer: IQuestionnaireResponse_Answer) => {
+                            handleAnswer(answer, linkId);
+                        };
+
+                        return generateFHIRForm(question, value, change);
                     })}
                     <div className={style.signature}>
                         <Undertittel>Dr. Nystøl</Undertittel>
--- apps/frontend/src/components/questionnaire/utils/generate-form.tsx
+++ apps/frontend/src/components/questionnaire/utils/generate-form.tsx
@@ -1,28 +1,37 @@
 import {
-    IQuestionnaire,
+    IQuestionnaireResponse_Answer,
     IQuestionnaire_Item,
     Questionnaire_ItemTypeKind,
 } from "@ahryman40k/ts-fhir-types/lib/R4";
 import React from "react";
-import {
-    Checkbox,
-    Input,
-    Label,
-    Radio,
-    RadioGruppe,
-    Select,
-    TextareaControlled,
-} from "nav-frontend-skjema";
-import AlertStripe from "nav-frontend-alertstriper";
+import { Checkbox, Input, Select, Textarea } from "nav-frontend-skjema";
+import { Datepicker } from "nav-datovelger/";
 import { Entry } from "../components/entry";
+import { Element } from "nav-frontend-typografi";
 
-export const generateFHIRForm = (question: IQuestionnaire_Item) => {
+import style from "../questionnaire.module.less";
+
+export const generateFHIRForm = (
+    question: IQuestionnaire_Item,
+    value: IQuestionnaireResponse_Answer[],
+    setValue: (answer: IQuestionnaireResponse_Answer) => void
+) => {
     switch (question.type) {
         case Questionnaire_ItemTypeKind._string:
-            return <Input type="text" label={question.text} key={question.linkId} />;
+            return (
+                <Input
+                    type="text"
+                    label={question.text}
+                    key={question.linkId}
+                    value={value.find((v) => v.valueString)?.valueString || ""}
+                    onChange={(e) => setValue({ valueString: e.target.value })}
+                />
+            );
         case Questionnaire_ItemTypeKind._text:
             return (
-                <TextareaControlled
+                <Textarea
+                    value={value.find((v) => v.valueString)?.valueString || ""}
+                    onChange={(e) => setValue({ valueString: e.target.value })}
                     label={question.text}
                     defaultValue={""}
                     maxLength={0}
@@ -32,6 +41,7 @@ export const generateFHIRForm = (question: IQuestionnaire_Item) => {
         case Questionnaire_ItemTypeKind._integer:
             return (
                 <Input
+                    // Missing: handle state
                     type="text"
                     inputMode="numeric"
                     pattern="[0-9]*"
@@ -39,9 +49,23 @@ export const generateFHIRForm = (question: IQuestionnaire_Item) => {
                     key={question.linkId}
                 />
             );
+        case Questionnaire_ItemTypeKind._date:
+            return (
+                <div className={style.dateInput}>
+                    <Element>{question.text}</Element>
+                    <Datepicker
+                        value={value.find((v) => v.valueDate)?.valueDate || ""}
+                        onChange={(value) => setValue({ valueDate: value })}
+                    />
+                </div>
+            );
         case Questionnaire_ItemTypeKind._choice:
             return (
-                <Select label={question.text} key={question.linkId}>
+                <Select
+                    label={question.text}
+                    key={question.linkId}
+                    value={value.find((v) => v.valueString)?.valueString || ""}
+                    onChange={(e) => setValue({ valueString: e.target.value })}>
                     <option value="">Velg</option>
                     {question.answerOption?.map((option, index) => {
                         return (
@@ -53,7 +77,14 @@ export const generateFHIRForm = (question: IQuestionnaire_Item) => {
                 </Select>
             );
         case Questionnaire_ItemTypeKind._boolean:
-            return <Checkbox label={question.text} key={question.linkId} />;
+            return (
+                <Checkbox
+                    label={question.text}
+                    key={question.linkId}
+                    checked={value.find((v) => v.valueBoolean)?.valueBoolean || false}
+                    onChange={(e) => setValue({ valueBoolean: e.target.checked })}
+                />
+            );
         case Questionnaire_ItemTypeKind._reference:
             return <Entry key={question.linkId} />;
         default:
