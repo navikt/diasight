b09e21e86e7167a3f8abfbb721c31b0a29e2f1b6
Michael Johansen <michael.johansen@nav.no>
2021-08-19 15:23:42 +0200

    updating dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-    projects: ["<rootDir>/apps/frontend", "<rootDir>/apps/api"],
-    transformIgnorePatterns: ['../../node_modules/(?!${nav-frontend-typografi})', "../../node_modules/(?!@navikt)"],
+const { getJestProjects } = require("@nrwl/jest");
 
+module.exports = {
+    transformIgnorePatterns: [
+        "../../node_modules/(?!${nav-frontend-typografi})",
+        "../../node_modules/(?!@navikt)",
+    ],
+    projects: getJestProjects(),
 };
